90ef58e761771f67fc1147f5c4a490c34ad81c0c
heisenbergthecoder <42775008+heisenbergthecoder@users.noreply.github.com>
2018-09-28 12:08:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+2018 Octopus Coin
+
+Octopus is a PoS-based cryptocurrency.
+
+Octopus is dependent upon libsecp256k1 by sipa, the sources for which can be found here: https://github.com/bitcoin/secp256k1
+
+Port: 15117 RPC Port: 15116
+
+Coin name: Octopus Coin 
+Coin abbreviation: OCTPS 
+Coin type: POS with POW used for pre-mine 
+Hashing algorithm: NeoScrypt 
+Difficult retargetting: Every 1 Block 
+Maximum block size: 3MB 
+Minimum stake age: 24 Hours 
+Maximum stake age: Unlimited 
+TX maturity: Mined 46 Confirmations 
+TX maturity: Sent/Received 10 Confirmations 
+Max supply: 200,000,000 OCTPS 
+Premined coins: 2% = 4,000,000 OCTPS
+Block time: 90 seconds 
+
+Masternode Collateral specifications
+
+15.000 (15) = 0,1%
+50.000 (60) = 0,12%
+120.000 (180) = 0,15%
+
+After block 157000 (New Masternode Collateral)
+30.000 (27) = 0,09%
+100.000 (110) = 0,11%
+240.000 (336) = 0,14%
+
+After block 242600
+30.000 (13,5) = 0,045%
+100.000 (55) = 0,055%
+240.000 (168) = 0,07%
---
+++ src/chainparams.cpp
@@ -0,0 +1,209 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "assert.h"
+
+#include "chainparams.h"
+#include "main.h"
+#include "util.h"
+
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+
+//
+// Main network
+//
+
+// Convert the pnSeeds array into usable address objects.
+static void convertSeeds(std::vector<CAddress> &vSeedsOut, const unsigned int *data, unsigned int count, int port)
+{
+     // It'll only connect to one or two seed nodes because once it connects,
+     // it'll get a pile of addresses with newer timestamps.
+     // Seed nodes are given a random 'last seen time' of between one and two
+     // weeks ago.
+     const int64_t nOneWeek = 7*24*60*60;
+    for (unsigned int k = 0; k < count; ++k)
+    {
+        struct in_addr ip;
+        unsigned int i = data[k], t;
+        
+        // -- convert to big endian
+        t =   (i & 0x000000ff) << 24u
+            | (i & 0x0000ff00) << 8u
+            | (i & 0x00ff0000) >> 8u
+            | (i & 0xff000000) >> 24u;
+        
+        memcpy(&ip, &t, sizeof(ip));
+        
+        CAddress addr(CService(ip, port));
+        addr.nTime = GetTime()-GetRand(nOneWeek)-nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0x4d;
+        pchMessageStart[1] = 0x41;
+        pchMessageStart[2] = 0x4a;
+        pchMessageStart[3] = 0x4c;
+        vAlertPubKey = ParseHex("047f4de20f3110f944588c2240a93e409e3f9b54e78443a095b2af475b6c3279bdcd9ded2b3cced180563f1ff2dff2767082c2be3fcf7ec68d893dd9f7af4fe947");
+        nDefaultPort = 15117;
+        nRPCPort = 15116;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+
+        const char* pszTimestamp = "October 7, 2018 Jair Bolsonaro will be president of Brazil";
+        std::vector<CTxIn> vin;
+        vin.resize(1);
+        vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        std::vector<CTxOut> vout;
+        vout.resize(1);
+        vout[0].SetEmpty();
+        CTransaction txNew(1, 1537575239, vin, vout, 0);
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1537575239;
+        genesis.nBits    = 520159231; 
+        genesis.nNonce   = 1734222;
+
+		
+		
+        hashGenesisBlock = genesis.GetHash();
+
+
+	//printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+	//printf("genesis.hashMerkleRoot = %s\n", genesis.hashMerkleRoot.ToString().c_str());
+
+
+
+        assert(hashGenesisBlock == uint256("0x1a22959b96a91d404b5c4d4434c34774d9a0c1e7fe947a43ed7e2ce6ea8396a3"));
+        assert(genesis.hashMerkleRoot == uint256("0x5e8058ec6b6b9c2944947b8341a2d9c406ef9358a7f4857da503442b40c5912a"));
+
+        
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,115);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,85);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,153);
+        base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1,40);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
+	
+	vSeeds.push_back(CDNSSeedData("0",  "201.73.79.39"));
+        vSeeds.push_back(CDNSSeedData("1",  "94.177.189.246"));
+        vSeeds.push_back(CDNSSeedData("2",  "80.211.72.155"));
+        
+	convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
+
+        nPoolMaxTransactions = 3;
+        //strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+        //strMasternodePaymentsPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
+        strDarksendPoolDummyAddress = "0x";
+        nLastPOWBlock = 2000;
+        nPOSStartBlock = 201;
+        nMasternodePaymentStartBlock = 50;
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet
+//
+
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0x4d;
+        pchMessageStart[1] = 0x41;
+        pchMessageStart[2] = 0x4a;
+        pchMessageStart[3] = 0x4c;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+        vAlertPubKey = ParseHex("047f4de20f3110f944588c2240a93e409e3f9b54e78443a095b2af475b6c3279bdcd9ded2b3cced180563f1ff2dff2767082c2be3fcf7ec68d893dd9f7af4fe947");
+        nDefaultPort = 15114;
+        nRPCPort = 15115;
+        strDataDir = "testnet";
+
+        // Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nBits  = 520159231; 
+        genesis.nNonce = 1734222;
+
+        assert(hashGenesisBlock == uint256("0x1a22959b96a91d404b5c4d4434c34774d9a0c1e7fe947a43ed7e2ce6ea8396a3"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,127);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+        base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,239);
+        base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1,40);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeeds(vFixedSeeds, pnTestnetSeed, ARRAYLEN(pnTestnetSeed), nDefaultPort);
+
+        nLastPOWBlock = 0x7fffffff;
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+    
+    bool fTestNet = GetBoolArg("-testnet", false);
+    
+    if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
